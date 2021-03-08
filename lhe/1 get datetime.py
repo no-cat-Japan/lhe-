b4f4a9002f61ca00d13c45e018f5fe0743d2c8a2
uno <munenori@jaist.ac.jp>
2021-03-08 14:45:29 +0900

    add new file

---
+++ lhe/1 get datetime.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import datetime
+d_today = datetime.date.today()
+td1 = datetime.timedelta(weeks=12)
+td2 = datetime.timedelta(weeks=1)
+print(td1-td2)
+
+
+# In[2]:
+
+
+d_today
+
+
+# In[8]:
+
+
+d_today.year
+
+
+# In[ ]:
+
+
+
+
